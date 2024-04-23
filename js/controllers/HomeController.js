324305d30bd45f5573f9d9fe0b6eb05af9dee214
Brett Lazarine <jblazarine@gmail.com>
2024-04-23 11:30:22 -0500

    add future sections, add footer, update css

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -3,7 +3,6 @@ app.controller('HomeController', ['$scope', 'movies', function($scope, movies) {
         $scope.movies = data;
     }).catch(function(error) {
         console.error('Error fetching movies:', error);
-        // Optionally, display an error message to the user
         $scope.errorMessage = 'An error occurred while fetching movies. Please try again later.';
     });
 }]);
